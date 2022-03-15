d09da6f482cb1c3f69e78c6fc36024a879e9efae
jacsonx <contatoforge@gmail.com>
2022-03-15 19:56:19 -0300

    initial commit

---
+++ src/main/java/com/javadocker/demo/api/controller/ServerController.java
@@ -0,0 +1,17 @@
+package com.javadocker.demo.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class ServerController {
+
+	@GetMapping
+	public String hello() {
+		return "Teste";
+	}
+	
+	
+}
